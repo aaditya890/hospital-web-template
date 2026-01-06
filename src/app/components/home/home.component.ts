8dd0202e69c2c27fe853d76d82d1909120e8be69
Rohit Nayak <rohit77.rn@gmail.com>
2026-01-06 19:02:58 +0530

    new changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TestimonialsComponent } from "../testimonials/testimonials.component";
 import { HeroSectionComponent } from '../../hero-section/hero-section.component';
+import { FaqComponent } from "../../faq/faq.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TestimonialsComponent, HeroSectionComponent],
+  imports: [TestimonialsComponent, HeroSectionComponent, FaqComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
